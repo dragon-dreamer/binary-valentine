7c47e75accfcda083ab4cfe315957ac7176bfd1f
Denis T <dragon-dreamer@users.noreply.github.com>
2024-02-14 23:06:53 +0000

    Update version.h

--- binary_valentine/include/binary_valentine/version.h
+++ binary_valentine/include/binary_valentine/version.h
@@ -10,6 +10,6 @@ constexpr std::string_view tool_name = "Binary Valentine";
 constexpr std::string_view tool_description = "Static analysis tool for executable files";
 constexpr std::string_view tool_website = "https://binary-valentine.com";
 constexpr std::string_view rule_info_uri = "/docs/rule/";
-constexpr std::array tool_version{ 0u, 2u, 0u, 0u };
+constexpr std::array tool_version{ 0u, 3u, 0u, 0u };
 
 } //namespace bv::version

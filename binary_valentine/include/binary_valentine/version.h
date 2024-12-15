b74a52dede3674df6dba45d7b976d9683fcfd9c8
Denis T <dragon-dreamer@users.noreply.github.com>
2024-12-15 22:40:59 +0000

    Add project GitHub URL to version.h

--- binary_valentine/include/binary_valentine/version.h
+++ binary_valentine/include/binary_valentine/version.h
@@ -10,6 +10,7 @@ constexpr std::string_view tool_name = "Binary Valentine";
 constexpr std::string_view tool_description = "Static analysis tool for executable files";
 constexpr std::string_view tool_website = "https://binary-valentine.com";
 constexpr std::string_view rule_info_uri = "/docs/rule/";
+constexpr std::string_view github_url = "https://github.com/dragon-dreamer/binary-valentine";
 constexpr std::array tool_version{ 0u, 3u, 1u, 0u };
 
 } //namespace bv::version

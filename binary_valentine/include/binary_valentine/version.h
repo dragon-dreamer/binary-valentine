468061875a8c338887435cb47724fd6c7fa82844
Denis T <dragon-dreamer@users.noreply.github.com>
2025-01-11 23:42:40 +0000

    Bump version to 0.4.0.0

--- binary_valentine/include/binary_valentine/version.h
+++ binary_valentine/include/binary_valentine/version.h
@@ -11,6 +11,6 @@ constexpr std::string_view tool_description = "Static analysis tool for executab
 constexpr std::string_view tool_website = "https://binary-valentine.com";
 constexpr std::string_view rule_info_uri = "/docs/rule/";
 constexpr std::string_view github_url = "https://github.com/dragon-dreamer/binary-valentine";
-constexpr std::array tool_version{ 0u, 3u, 1u, 0u };
+constexpr std::array tool_version{ 0u, 4u, 0u, 0u };
 
 } //namespace bv::version
